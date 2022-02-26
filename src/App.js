c5920ca2750638000802360bef559cf06652c67f
alexandravelascosvoboda <alex.v.s@live.com>
2022-02-26 17:01:11 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -6,27 +6,26 @@ function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-        <h1>Weather App</h1>
         <Weather />
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://inspiring-lewin-d06e19.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Alexandra Velasco-Svoboda
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/alexandra-vs/weather-react"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
       </div>
-      <footer>
-        This project was coded by {""}
-        <a
-          href="https://inspiring-lewin-d06e19.netlify.app/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Alexandra Velasco-Svoboda
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/alexandra-vs/weather-react"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,51 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import "./Weather.css";
+import "./index.css";
 
 export default function Weather() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
-
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "a64b705a37558d573aa48063a58b50cf";
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiURL).then(displayWeather);
-  }
-
-  function updateCitiy(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCitiy}
-      />
-      <button type="Submit">Search</button>
-    </form>
-  );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+      </div>
+      <div className="col-6">
         <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
+          <li>Preciptation:15%</li>
+          <li>Humidity: 72%</li>
+          <li>Wind: 13 km/h</li>
         </ul>
       </div>
-    );
-  } else {
-    return form;
-  }
+    </div>
+  );
 }
