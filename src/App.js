bfe63bea78008bad423d1052d3f0e79ad26024cf
alexandravelascosvoboda <alex.v.s@live.com>
2022-02-26 15:23:08 +0100

    Added Bootstrap + Updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React from "react";
 import Weather from "./Weather";
-import "./styles.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="container-fluid App">
-      <h1>Weather App</h1>
-      <Weather />
+    <div className="App">
+      <div className="container-fluid">
+        <h1>Weather App</h1>
+        <Weather />
+      </div>
+      <footer>
+        This project was coded by {""}
+        <a
+          href="https://inspiring-lewin-d06e19.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Alexandra Velasco-Svoboda
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/alexandra-vs/weather-react"
+          rel="noreferrer"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
