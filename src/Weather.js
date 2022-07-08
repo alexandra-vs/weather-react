cfce4443468ccfb0f32c648d9195420f2542877b
alexandravelascosvoboda <alex.v.s@live.com>
2022-07-08 18:11:36 +1000

    css

--- src/Weather.js
+++ src/Weather.js
@@ -51,7 +51,7 @@ export default function Weather(props) {
               <input
                 type="search"
                 placeholder="Enter a city..."
-                className="form-control"
+                className="form-control the-form"
                 autoFocus="on"
                 onChange={handleCityChange}
               />
--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="black"
+      color="#303c6c"
       size={props.size}
       animate={true}
     />
